71a93a6f088c8f63046a089ea8adb56bf183b3a9
Soo186 <rlagustn9797@gmail.com>
2025-04-14 14:36:03 +0900

    chore : 초기 환경 세팅

---
+++ src/main/java/com/popwine/popwine/PopWineApplication.java
@@ -0,0 +1,13 @@
+package com.popwine.popwine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopWineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopWineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/popwine/popwine/PopWineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popwine.popwine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopWineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

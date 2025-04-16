4e0c8b5dbd99541d044c242af6bd92295122abac
Soo186 <rlagustn9797@gmail.com>
2025-04-16 19:40:13 +0900

    chore : 기본 package 구조 잡기

---
+++ src/main/java/com/popwine/module/order/application/OrderService.java
@@ -0,0 +1,7 @@
+package com.popwine.module.order.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+}
---
+++ src/main/java/com/popwine/module/order/controller/dto/OrderRequest.java
@@ -0,0 +1,4 @@
+package com.popwine.module.order.controller.dto;
+
+public class OrderRequest {
+}

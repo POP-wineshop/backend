41c292e7fd69cdec19b5776bd55b7d07402d0d55
Soo186 <rlagustn9797@gmail.com>
2025-04-23 17:33:20 +0900

    feat: 에러 코드 enum 정의

---
+++ src/main/java/com/popwine/core/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.popwine.core.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    INVALID_INPUT(400, "잘못된 요청입니다"),
+    ENTITY_NOT_FOUND(404, "존재하지 않는 리소스입니다."),
+    INTERNAL_SERVER_ERROR(500, "서버 오류입니다.");
+
+
+    private final int status;
+    private final String message;
+
+}

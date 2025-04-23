947fe5e2da68e66e9ad3c46ef9c50a23131fc3a1
Soo186 <rlagustn9797@gmail.com>
2025-04-23 17:34:03 +0900

    feat: 에러 응답 dto 생성

---
+++ src/main/java/com/popwine/core/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.popwine.core.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+
+    public static ErrorResponse from(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.getStatus(), errorCode.getMessage());
+    }
+
+    public static ErrorResponse from(BaseException exception) {
+        return new ErrorResponse(exception.getErrorCode().getStatus(), exception.getMessage());
+    }
+}

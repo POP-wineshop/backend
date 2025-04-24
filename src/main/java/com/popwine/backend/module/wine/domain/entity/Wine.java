0dffdcc24450dc03765136c56da4535399c0be3b
Soo186 <rlagustn9797@gmail.com>
2025-04-24 23:48:44 +0900

    feat: 엔티티 필드에 대한 설명 추가, 타닌 제거

--- src/main/java/com/popwine/backend/module/wine/domain/entity/Wine.java
+++ src/main/java/com/popwine/backend/module/wine/domain/entity/Wine.java
@@ -20,41 +20,51 @@ public class Wine {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //와인 이름
     @Embedded
     private WineName name;
 
+    //와인 가격
     @Embedded
     private Price price;
 
+    //와인 빈티지
     private int vintage;
 
+    //와인 원산지
     private String origin;
 
+    //와인 카테고리
     @ElementCollection
     @CollectionTable(name = "wine_category", joinColumns = @JoinColumn(name = "wine_id"))
     @Column(name = "category_id")
     private List<Long> categoryIDs;
+
+    //와인 포도 품종
     private String grapeVariety;
 
+
+    //와인 종류
     @Enumerated(EnumType.STRING)
     private WineType wineType;
 
+
+    //와인 지역
     private String region;
+    //와인 제조사
     private String winery;
+    //와인 알콜 도수
     private String alcoholContent;
+    //와인 테이스팅노트
     private String tastingNote;
+    //와인 페어링
     private String foodPairing;
+    //와인 설명
     private String description;
 
-    @Embedded//당도, 산도, 타닌, 바디
+    @Embedded//당도, 산도, 바디
     private TasteProfile tasteProfile;
 
+    //와인 이미지 URL
     private String imageUrl;
-
-    public void addCategory(Long categoryId) {
-        if (categoryId == null) {
-            throw new IllegalArgumentException("카테고리 ID는 null일 수 없습니다.");
-        }
-        this.categoryIDs.add(categoryId);
-    }
 }

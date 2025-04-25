dc3fc7404adab176b5fd80a519a70e25045e561e
Soo186 <rlagustn9797@gmail.com>
2025-04-25 16:57:47 +0900

    refactor: 와인 등록 시 카테고리 저장 메소드

--- src/main/java/com/popwine/backend/module/wine/domain/repository/CategoryRepository.java
+++ src/main/java/com/popwine/backend/module/wine/domain/repository/CategoryRepository.java
@@ -1,12 +1,14 @@
 package com.popwine.backend.module.wine.domain.repository;
 
 import com.popwine.backend.module.wine.domain.entity.Category;
+import com.popwine.backend.module.wine.domain.enums.CategoryType;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CategoryRepository  {
-    Optional<Category> findById(Long id);
-    boolean existsById(Long id);
+    Optional<Category> findByNameAndType(String name, CategoryType type);
+    Category save(Category category);
+
 }

4834e7e75a5073f837049cd567dbded390199e51
Soo186 <rlagustn9797@gmail.com>
2025-04-26 17:28:28 +0900

    refactor : Service description 추가

--- src/test/java/com/popwine/WineServiceTest.java
+++ src/test/java/com/popwine/WineServiceTest.java
@@ -1,7 +1,8 @@
 package com.popwine;
 
 import com.popwine.backend.module.wine.application.WineService;
-import com.popwine.backend.module.wine.controller.WineResponseDto;
+import com.popwine.backend.module.wine.controller.dto.WineRequestDto;
+import com.popwine.backend.module.wine.controller.dto.WineResponseDto;
 import com.popwine.backend.module.wine.domain.entity.Wine;
 import com.popwine.backend.module.wine.domain.repository.WineRepository;
 import com.popwine.backend.module.wine.domain.vo.Price;
@@ -13,11 +14,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.Description;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class WineServiceTest {
@@ -29,6 +32,7 @@ class WineServiceTest {
     private WineService wineService;
 
     @Test
+    @Description("모든 와인 조회")
     void testGetAllWine() {
         // Given
         Wine wine1 = Wine.builder()
@@ -65,6 +69,7 @@ class WineServiceTest {
     }
 
     @Test
+    @Description("카테고리 와인 조회")
     void testGetWinesByCategory() {
         // Given
         List<Long> categoryIds = List.of(1L, 2L);
@@ -93,6 +98,7 @@ class WineServiceTest {
     }
 
     @Test
+    @Description("와인 ID통해 상세정보 조회")
     void testGetWineById() {
         // Given
         Long wineId = 1L;
@@ -123,5 +129,46 @@ class WineServiceTest {
         assertEquals("Cabernet Sauvignon", result.getGrapeVariety());
         assertEquals("RED", result.getWineType().name());
         assertEquals("https://example.com/image.jpg", result.getImageUrl());
+    }
+
+    @Test
+    @Description("와인 등록")
+    void testCreateWine() {
+
+        // Given
+        WineRequestDto wineRequestDto = WineRequestDto.builder()
+                .wineName(new WineName("샤또 마고", "Chateau Margaux"))
+                .price(new Price(150000))
+                .vintage(2020)
+                .country("France")
+                .region("Bordeaux")
+                .grapeVariety("Cabernet Sauvignon")
+                .wineType(WineType.RED)
+                .tasteProfile(new TasteProfile(3, 3, 3))
+                .imageUrl("https://example.com/image.jpg")
+                .build();
+
+        Wine wine = Wine.builder()
+                .id(1L)
+                .name(new WineName("샤또 마고", "Chateau Margaux"))
+                .price(new Price(150000))
+                .vintage(2020)
+                .country("France")
+                .region("Bordeaux")
+                .grapeVariety("Cabernet Sauvignon")
+                .wineType(WineType.RED)
+                .tasteProfile(new TasteProfile(3, 3, 3))
+                .imageUrl("https://example.com/image.jpg")
+                .build();
+
+        when(wineRepository.save(any(Wine.class))).thenReturn(wine);
+
+
+        // When
+        WineResponseDto result = wineService.createWine(wineRequestDto);
+
+        // Then
+        assertEquals(wine.getId(), result.getId());
+        assertEquals(wine.getPrice().getValue(), result.getPrice());
     }
 }

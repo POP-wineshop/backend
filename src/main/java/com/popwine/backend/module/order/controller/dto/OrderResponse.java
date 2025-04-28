8411593227803cf32e3fdd47f266bea6a5d8b90c
Soo186 <rlagustn9797@gmail.com>
2025-04-28 19:35:31 +0900

    feat: 주문 정보 response 생성

--- src/main/java/com/popwine/backend/module/order/controller/dto/OrderResponse.java
+++ src/main/java/com/popwine/backend/module/order/controller/dto/OrderResponse.java
@@ -1,9 +1,31 @@
 package com.popwine.backend.module.order.controller.dto;
 
+import com.popwine.backend.module.order.domain.entity.Order;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
 @AllArgsConstructor
 public class OrderResponse {
+
+    private Long orderId;
+    private String ordererName;
+    private String orderStatus;
+    private List<OrderItemResponse> orderItems;
+
+    public static OrderResponse from(Order order) {
+        List<OrderItemResponse> orderItemResponses = order.getOrderItems().stream()
+                .map(OrderItemResponse::from)
+                .collect(Collectors.toList());
+
+        return new OrderResponse(
+                order.getId(),
+                order.getOrdererName(),
+                order.getOrderstatus().name(),
+                orderItemResponses
+        );
+    }
 }

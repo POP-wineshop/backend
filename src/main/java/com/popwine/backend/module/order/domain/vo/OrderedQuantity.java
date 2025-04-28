3193a76a019c60e9be2822a094cab5713191fd1c
Soo186 <rlagustn9797@gmail.com>
2025-04-28 19:34:28 +0900

    feat : 주문 유효성 검사 Vo 생성

--- src/main/java/com/popwine/backend/module/order/domain/vo/OrderedQuantity.java
+++ src/main/java/com/popwine/backend/module/order/domain/vo/OrderedQuantity.java
@@ -9,14 +9,19 @@ import lombok.NoArgsConstructor;
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Quantity {
+public class OrderedQuantity {
 
     private int value;
 
-    public Quantity(int value) {
+    public OrderedQuantity(int value) {
         if (value <= 0) {
             throw new IllegalArgumentException("수량은 0보다 작거나 같을 수 없습니다.");
         }
         this.value = value;
     }
+
+    public static OrderedQuantity of(int value) {
+        return new OrderedQuantity(value);
+    }
+
 }

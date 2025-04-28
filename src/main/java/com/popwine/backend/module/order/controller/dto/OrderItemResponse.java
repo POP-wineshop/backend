71ebd9b89e809dc746063776224606633aa4bb9f
Soo186 <rlagustn9797@gmail.com>
2025-04-28 19:35:14 +0900

    feat  : 주문 상품 정보 response 생성

---
+++ src/main/java/com/popwine/backend/module/order/controller/dto/OrderItemResponse.java
@@ -0,0 +1,28 @@
+package com.popwine.backend.module.order.controller.dto;
+
+import com.popwine.backend.module.order.domain.vo.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrderItemResponse {
+
+    private Long wineId;
+    private String wineNameKor;
+    private int winePrice;
+    private int orderedQuantity;
+    private int orderedPrice;
+    private String wineImageUrl;
+
+    public static OrderItemResponse from(OrderItem item) {
+        return new OrderItemResponse(
+                item.getWineId(),
+                item.getWineNameKor(),
+                item.getWinePrice(),
+                item.getOrderedQuantity().getValue(),
+                item.getOrderedPrice().getValue(),
+                item.getWineImageUrl()
+        );
+    }
+}

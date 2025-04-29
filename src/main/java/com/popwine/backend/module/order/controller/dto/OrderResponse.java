b338851ac56d0e7f8e8a171b2635bfc4ac3c68cc
Soo186 <rlagustn9797@gmail.com>
2025-04-29 14:19:00 +0900

    feat: 총 합계 response에 추가

--- src/main/java/com/popwine/backend/module/order/controller/dto/OrderResponse.java
+++ src/main/java/com/popwine/backend/module/order/controller/dto/OrderResponse.java
@@ -2,30 +2,39 @@ package com.popwine.backend.module.order.controller.dto;
 
 import com.popwine.backend.module.order.domain.entity.Order;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class OrderResponse {
 
     private Long orderId;
-    private String ordererName;
+    // private String ordererName; 추후 예정
     private String orderStatus;
     private List<OrderItemResponse> orderItems;
+    private int totalPrice;
 
     public static OrderResponse from(Order order) {
         List<OrderItemResponse> orderItemResponses = order.getOrderItems().stream()
                 .map(OrderItemResponse::from)
                 .collect(Collectors.toList());
 
-        return new OrderResponse(
-                order.getId(),
-                order.getOrdererName(),
-                order.getOrderstatus().name(),
-                orderItemResponses
-        );
+
+        //총 합계
+        int totalPrice = orderItemResponses.stream()
+                .mapToInt(OrderItemResponse::getOrderedPrice)
+                .sum();
+
+        return OrderResponse.builder()
+                .orderId(order.getId())
+                .orderStatus(order.getOrderstatus().name())
+                .orderItems(orderItemResponses)
+                .totalPrice(totalPrice)
+                .build();
     }
 }

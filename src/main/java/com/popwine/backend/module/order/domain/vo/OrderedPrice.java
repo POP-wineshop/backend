d632159c20216eeeefe4a3ba4ec5e458ae6551f3
Soo186 <rlagustn9797@gmail.com>
2025-04-29 14:16:54 +0900

    feat : 주문 가격 Column 명 명시

--- src/main/java/com/popwine/backend/module/order/domain/vo/OrderedPrice.java
+++ src/main/java/com/popwine/backend/module/order/domain/vo/OrderedPrice.java
@@ -1,6 +1,7 @@
 package com.popwine.backend.module.order.domain.vo;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -11,14 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderedPrice {
 
+    @Column(name = "ordered_price")
     private int value;
 
     public OrderedPrice(int winePrice, OrderedQuantity quantity) {
 
-        if (winePrice < 0 || quantity.getValue() <= 0) {
+        if (winePrice < 0 || quantity.getQuantity() <= 0) {
             throw new IllegalArgumentException("가격과 수량은 0보다 작을 수 없습니다.");
         }
-        this.value = winePrice * quantity.getValue();
+        this.value = winePrice * quantity.getQuantity();
         }
 
     public static OrderedPrice of(int winePrice, OrderedQuantity quantity) {

976dcb6c660a36684792e5f93f4a82c36ff06dad
Soo186 <rlagustn9797@gmail.com>
2025-04-30 13:13:44 +0900

    refactor : 예외 메시지 변경

--- src/main/java/com/popwine/backend/module/wine/domain/vo/WineName.java
+++ src/main/java/com/popwine/backend/module/wine/domain/vo/WineName.java
@@ -13,10 +13,10 @@ public class WineName {
 
     public WineName(String korean, String english) {
         if (korean == null || korean.isEmpty()) {
-            throw new IllegalArgumentException("와인 이름(한글)은 null 또는 공백일 수 없습니다.");
+            throw new IllegalArgumentException("와인 이름(한글)은 null 또는 공백일 수 없다.");
         }
         if (english == null || english.isEmpty()) {
-            throw new IllegalArgumentException("와인 이름(영어)은 null 또는 공백일 수 없습니다.");
+            throw new IllegalArgumentException("와인 이름(영어)은 null 또는 공백일 수 없다.");
         }
         this.korean = korean;
         this.english = english;

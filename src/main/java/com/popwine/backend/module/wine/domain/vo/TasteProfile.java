5c7483253f3aed64044e5d25298c1b54966240b3
Soo186 <rlagustn9797@gmail.com>
2025-04-30 13:14:12 +0900

    feat : 맛 정보 유효성 검증 메소드 추가

--- src/main/java/com/popwine/backend/module/wine/domain/vo/TasteProfile.java
+++ src/main/java/com/popwine/backend/module/wine/domain/vo/TasteProfile.java
@@ -13,8 +13,18 @@ public class TasteProfile {
     private int body;
 
     public TasteProfile(int sweetness, int acidity,  int body) {
+        validate("sweetness", sweetness);
+        validate("acidity", acidity);
+        validate("body", body);
+
         this.sweetness = sweetness;
         this.acidity = acidity;
         this.body = body;
+    }
+
+    public void validate(String name, int value) {
+        if (value<1 || value>5) {
+            throw new IllegalArgumentException(name+"는 1 이상 5 이하여야 한다.");
+        }
     }
 }

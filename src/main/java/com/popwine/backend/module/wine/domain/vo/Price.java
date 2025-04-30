e4a4102f563be1f0a109f5c63d1a7c94ac2c0284
Soo186 <rlagustn9797@gmail.com>
2025-04-30 13:14:30 +0900

    refactor : not null 명시, 예외 추가

--- src/main/java/com/popwine/backend/module/wine/domain/vo/Price.java
+++ src/main/java/com/popwine/backend/module/wine/domain/vo/Price.java
@@ -1,5 +1,6 @@
 package com.popwine.backend.module.wine.domain.vo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,11 +9,16 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class Price {
-    int value;
+
+    @Column(nullable = false)
+    private int value;
 
     public Price(int value) {
         if (value < 0) {
-            throw new IllegalArgumentException("가격은 0보다 작을 수 없습니다.");
+            throw new IllegalArgumentException("가격은 0 이상이어야 한다.");
+        }
+        if (value > 1_000_000_000) {
+            throw new IllegalArgumentException("가격은 10억 이하여야 한다.");
         }
         this.value = value;
     }

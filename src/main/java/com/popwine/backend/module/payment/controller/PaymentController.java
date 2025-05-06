64b62048003e8d8b4ee42cf5365d7ac9b197e20f
Soo186 <rlagustn9797@gmail.com>
2025-05-06 13:07:32 +0900

    feat(payment): PaymentController에 /{orderId}/confirm API 구현

--- src/main/java/com/popwine/backend/module/payment/controller/PaymentController.java
+++ src/main/java/com/popwine/backend/module/payment/controller/PaymentController.java
@@ -1,59 +1,28 @@
 package com.popwine.backend.module.payment.controller;
 
 
+import com.popwine.backend.module.payment.application.PaymentService;
 import com.popwine.backend.module.payment.controller.dto.PaymentConfirmRequest;
-import org.springframework.http.HttpStatus;
+import com.popwine.backend.module.payment.controller.dto.PaymentConfirmResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import org.json.simple.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 
 @RestController
-@RequestMapping("/api/payment")
+@RequiredArgsConstructor
+@RequestMapping("/api/payments")
 public class PaymentController {
 
-    @PostMapping("/confirm")
-    public ResponseEntity<?> confirmPayment(@RequestBody PaymentConfirmRequest request) {
-        String secretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6"; //테스트키 환경변수 처리 X
-        String encodedAuth = "Basic " + Base64.getEncoder().encodeToString((secretKey + ":").getBytes());
+    private final PaymentService paymentService;
 
-        JSONObject payload = new JSONObject();
-        payload.put("paymentKey", request.getPaymentKey());
-        payload.put("orderId", request.getOrderId());
-        payload.put("amount", request.getAmount());
-
-        try {
-            URL url = new URL("https://api.tosspayments.com/v1/payments/confirm");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Authorization", encodedAuth);
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setDoOutput(true);
-
-            OutputStream os = conn.getOutputStream();
-            os.write(payload.toJSONString().getBytes(StandardCharsets.UTF_8));
-            os.flush();
-
-            int responseCode = conn.getResponseCode();
-            InputStream is = (responseCode == 200) ? conn.getInputStream() : conn.getErrorStream();
-            String result = new BufferedReader(new InputStreamReader(is)).lines().reduce("", String::concat);
-
-            return ResponseEntity.status(responseCode).body(result);
-
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("토스 결제 승인 실패: " + e.getMessage());
-        }
+    @PostMapping("/{orderId}/confirm")
+    public ResponseEntity<PaymentConfirmResponse> confirmPayment(
+            @RequestBody PaymentConfirmRequest request,
+            @PathVariable("orderId") Long orderId
+    ) {
+        PaymentConfirmResponse response = paymentService.confirmPayment(request, orderId);
+        return ResponseEntity.ok(response);
     }
 }

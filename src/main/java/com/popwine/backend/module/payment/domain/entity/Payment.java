1c2324dfec2bf671f0854f767222220f12ed561f
Soo186 <rlagustn9797@gmail.com>
2025-05-06 13:08:33 +0900

    feat(payment): Payment 엔티티 및 PaymentStatus enum 작성

---
+++ src/main/java/com/popwine/backend/module/payment/domain/entity/Payment.java
@@ -0,0 +1,28 @@
+package com.popwine.backend.module.payment.domain.entity;
+
+
+import com.popwine.backend.module.payment.domain.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long reservationId;
+    private String paymentKey;
+    private int amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    private String approvedAt;
+
+}
---
+++ src/main/java/com/popwine/backend/module/payment/domain/enums/PaymentStatus.java
@@ -0,0 +1,18 @@
+package com.popwine.backend.module.payment.domain.enums;
+
+public enum PaymentStatus {
+    READY("결제 준비"),
+    COMPLETED("결제 완료"),
+    FAILED("결제 실패"),
+    CANCELLED("결제 취소");
+
+    private final String description;
+
+    PaymentStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}

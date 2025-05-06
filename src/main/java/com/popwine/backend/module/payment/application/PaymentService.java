0d21fb7f84fe284f779f4f5a3b3813f0befdf3c7
Soo186 <rlagustn9797@gmail.com>
2025-05-06 13:07:12 +0900

    PaymentService에 PgProcessor 활용 결제 승인 구현

---
+++ src/main/java/com/popwine/backend/module/payment/application/PaymentService.java
@@ -0,0 +1,35 @@
+package com.popwine.backend.module.payment.application;
+
+import com.popwine.backend.module.payment.controller.dto.PaymentConfirmRequest;
+import com.popwine.backend.module.payment.controller.dto.PaymentConfirmResponse;
+import com.popwine.backend.module.payment.domain.entity.Payment;
+import com.popwine.backend.module.payment.domain.enums.PaymentStatus;
+import com.popwine.backend.module.payment.domain.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PgPaymentProcessor paymentProcessor;
+    private final PaymentRepository paymentRepository;
+
+    public PaymentConfirmResponse confirmPayment(PaymentConfirmRequest request, Long orderId) {
+        // PGProcessor를 통해 결제 승인 처리
+        PaymentConfirmResponse response = paymentProcessor.confirmPayment(request);
+
+        // 응답을 Payment 엔티티로 저장
+        Payment payment = Payment.builder()
+                .id(orderId)
+                .paymentKey(response.getPaymentKey())
+                .amount(response.getAmount())
+                .status(PaymentStatus.valueOf(response.getStatus()))
+                .approvedAt(response.getApprovedAt())
+                .build();
+
+        paymentRepository.save(payment);
+        return response;
+    }
+}

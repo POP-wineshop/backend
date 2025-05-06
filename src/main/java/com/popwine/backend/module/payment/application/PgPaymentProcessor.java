899bbd19e243bdc08225ce9987dfd16158bae37c
Soo186 <rlagustn9797@gmail.com>
2025-05-06 16:14:45 +0900

    fix(payment): 인터페이스에서 @Component 제거해서 중복 빈 에러 해결

--- src/main/java/com/popwine/backend/module/payment/application/PgPaymentProcessor.java
+++ src/main/java/com/popwine/backend/module/payment/application/PgPaymentProcessor.java
@@ -2,9 +2,8 @@ package com.popwine.backend.module.payment.application;
 
 import com.popwine.backend.module.payment.controller.dto.PaymentConfirmRequest;
 import com.popwine.backend.module.payment.controller.dto.PaymentConfirmResponse;
-import org.springframework.stereotype.Component;
 
-@Component
+
 public interface PgPaymentProcessor {
     PaymentConfirmResponse confirmPayment(PaymentConfirmRequest request);
 }

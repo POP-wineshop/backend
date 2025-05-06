a2c16841ede7514675c2a2158e5f212bddeeeaa5
Soo186 <rlagustn9797@gmail.com>
2025-05-06 13:08:21 +0900

    feat(payment): PaymentConfirmRequest/Response DTO 정의

--- src/main/java/com/popwine/backend/module/payment/controller/dto/PaymentConfirmRequest.java
+++ src/main/java/com/popwine/backend/module/payment/controller/dto/PaymentConfirmRequest.java
@@ -1,11 +1,17 @@
 package com.popwine.backend.module.payment.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentConfirmRequest {
     private String paymentKey;
     private String orderId;
     private String amount;
-    // getter/setter
+
+
+
 }
---
+++ src/main/java/com/popwine/backend/module/payment/controller/dto/PaymentConfirmResponse.java
@@ -0,0 +1,31 @@
+package com.popwine.backend.module.payment.controller.dto;
+
+import com.popwine.backend.module.payment.domain.entity.Payment;
+import com.popwine.backend.module.payment.domain.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentConfirmResponse {
+    private String orderId;
+    private String paymentKey;
+    private int amount;
+    private String status;
+    private String approvedAt;
+
+
+    public Payment toEntity(Long orderId) {
+        return Payment.builder()
+                .reservationId(orderId)
+                .paymentKey(paymentKey)
+                .amount(amount)
+                .status(PaymentStatus.valueOf(status))
+                .approvedAt(approvedAt)
+                .build();
+    }
+}

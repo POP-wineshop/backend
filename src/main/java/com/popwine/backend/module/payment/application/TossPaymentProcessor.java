3bfd2573b3deb914f33d0019068999a4b01353a5
Soo186 <rlagustn9797@gmail.com>
2025-05-07 10:05:55 +0900

    refactor : component - >service로 직관적으로 어노테이션 변경

--- src/main/java/com/popwine/backend/module/payment/application/TossPaymentProcessor.java
+++ src/main/java/com/popwine/backend/module/payment/application/TossPaymentProcessor.java
@@ -4,7 +4,6 @@ import com.popwine.backend.module.payment.controller.dto.PaymentConfirmRequest;
 import com.popwine.backend.module.payment.controller.dto.PaymentConfirmResponse;
 import com.popwine.backend.module.payment.infrastructure.client.TossPaymentClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service

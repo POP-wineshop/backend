4c72e166a2b9f3f227980f9bc743814aa81550f0
Soo186 <rlagustn9797@gmail.com>
2025-05-10 14:37:57 +0900

    refactor : http 상태 코드 명시로 인한 설정이였지만, 중복된 역할 하므로 어노테이션 제거

--- src/main/java/com/popwine/backend/core/exception/GlobalExceptionHandler.java
+++ src/main/java/com/popwine/backend/core/exception/GlobalExceptionHandler.java
@@ -8,14 +8,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleBaseException(BaseException exception) {
         ErrorResponse errorResponse = ErrorResponse.from(exception);
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception exception) {
         ErrorResponse errorResponse = ErrorResponse.from(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));

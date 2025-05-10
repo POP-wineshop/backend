324228955b0c73ca672c8f21e9339caaa78a9dc9
Soo186 <rlagustn9797@gmail.com>
2025-05-10 21:55:47 +0900

    refactor : ApI응답값 통일

--- src/main/java/com/popwine/backend/module/payment/controller/PaymentController.java
+++ src/main/java/com/popwine/backend/module/payment/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.popwine.backend.module.payment.controller;
 
 
+import com.popwine.backend.core.response.ApiResponse;
 import com.popwine.backend.module.payment.application.PaymentService;
 import com.popwine.backend.module.payment.controller.dto.PaymentConfirmRequest;
 import com.popwine.backend.module.payment.controller.dto.PaymentConfirmResponse;
@@ -17,12 +18,20 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
+    /**
+     * 결제 승인 요청
+     *
+     * @param request 결제 승인 요청 정보
+     * @param orderId 주문 ID
+     * @return 결제 승인 응답
+     */
+
     @PostMapping("/{orderId}/confirm")
-    public ResponseEntity<PaymentConfirmResponse> confirmPayment(
+    public ApiResponse<PaymentConfirmResponse> confirmPayment(
             @RequestBody PaymentConfirmRequest request,
             @PathVariable("orderId") Long  orderId
     ) {
         PaymentConfirmResponse response = paymentService.confirmPayment(request, orderId);
-        return ResponseEntity.ok(response);
+        return ApiResponse.success(response);
     }
 }

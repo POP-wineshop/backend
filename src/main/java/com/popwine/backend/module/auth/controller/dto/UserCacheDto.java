b6fe720247ac43deb39d9b4e041734970612ab47
Soo186 <rlagustn9797@gmail.com>
2025-05-13 14:31:35 +0900

    feat : 팩토리 from 메서드 추가

--- src/main/java/com/popwine/backend/module/auth/controller/dto/UserCacheDto.java
+++ src/main/java/com/popwine/backend/module/auth/controller/dto/UserCacheDto.java
@@ -1,5 +1,6 @@
 package com.popwine.backend.module.auth.controller.dto;
 
+import com.popwine.backend.module.auth.domain.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +14,13 @@ import java.io.Serializable;
 @Builder
 public class UserCacheDto implements Serializable {
 
-    private Long id;
+    private Long userId;
     private String username;
+
+    public static UserCacheDto from(User user) {
+        return UserCacheDto.builder()
+                .userId(user.getId())
+                .username(user.getUsername())
+                .build();
+    }
 }

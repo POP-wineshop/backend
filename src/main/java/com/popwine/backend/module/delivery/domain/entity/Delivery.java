46e20aaff69b5f80032c6ebb2343c823dd2881f9
Soo186 <rlagustn9797@gmail.com>
2025-05-16 17:17:31 +0900

    feat : Delivery entity 추가

---
+++ src/main/java/com/popwine/backend/module/delivery/domain/entity/Delivery.java
@@ -0,0 +1,42 @@
+package com.popwine.backend.module.delivery.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId; // 사용자 ID
+
+    private String address; // 배송지 주소
+
+    private String detailAddress; // 상세 주소
+
+    private String recipientName; // 수령인 이름
+
+    private String recipientPhoneNumber; // 수령인 전화번호
+
+    private String deliveryMessage; // 배송 메시지
+
+    private boolean isDefault; // 기본 배송지 여부
+
+
+    //빌더처리
+    public static Delivery of(Long userId, String address, String detailAddress, String recipientName, String recipientPhoneNumber, String deliveryMessage, boolean isDefault) {
+        return new Delivery(null, userId, address, detailAddress, recipientName, recipientPhoneNumber, deliveryMessage, isDefault);
+    }
+}

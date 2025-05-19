4c769f986d938d125075ee18fd224626da95c3fa
Soo186 <rlagustn9797@gmail.com>
2025-05-19 23:06:47 +0900

    refactor : patch allowedMethod에 추가

--- src/main/java/com/popwine/backend/config/WebConfig.java
+++ src/main/java/com/popwine/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173") // React 앱의 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }

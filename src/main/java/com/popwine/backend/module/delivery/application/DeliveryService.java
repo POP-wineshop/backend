53e5d7cca53ca9cc551763821771eb40cec30d65
Soo186 <rlagustn9797@gmail.com>
2025-05-19 16:04:08 +0900

    feat :등록 조회 , Controller,service 메서드 추가

--- src/main/java/com/popwine/backend/module/delivery/application/DeliveryService.java
+++ src/main/java/com/popwine/backend/module/delivery/application/DeliveryService.java
@@ -5,10 +5,14 @@ import com.popwine.backend.module.delivery.controller.dto.DeliveryResponseDto;
 import com.popwine.backend.module.delivery.domain.entity.Delivery;
 import com.popwine.backend.module.delivery.domain.repository.DeliveryRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DeliveryService {
+
+    private final DeliveryRepository deliveryRepository;
 
     // 배송지 등록
     @Transactional
@@ -19,6 +23,12 @@ public class DeliveryService {
     }
 
     // 배송지 조회
+    @Transactional
+    public DeliveryResponseDto getDelivery(Long id) {
+        Delivery delivery = deliveryRepository.findById(id); // 배송지 조회
+        return DeliveryResponseDto.from(delivery); // Delivery -> DeliveryResponseDto 변환 후 반환
+    }
+
     // 배송지 수정
     // 배송지 삭제 상태만 변경
     // 기본 배송지 조회
--- src/main/java/com/popwine/backend/module/delivery/controller/DeliveryController.java
+++ src/main/java/com/popwine/backend/module/delivery/controller/DeliveryController.java
@@ -1,15 +1,32 @@
 package com.popwine.backend.module.delivery.controller;
 
+import com.popwine.backend.core.common.ApiResponse;
+import com.popwine.backend.module.delivery.application.DeliveryService;
+import com.popwine.backend.module.delivery.controller.dto.DeliveryRequestDto;
+import com.popwine.backend.module.delivery.controller.dto.DeliveryResponseDto;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/delivery")
+@RequiredArgsConstructor
 public class DeliveryController {
+
+    private final DeliveryService deliveryService;
     // 배송지 등록
 
+    public ApiResponse<DeliveryResponseDto> createDelivery(DeliveryRequestDto requestDto) {
+        DeliveryResponseDto response = deliveryService.createDelivery(requestDto);
+        return ApiResponse.success(response);
+    }
 
     // 배송지 조회
+    public ApiResponse<DeliveryResponseDto> getDelivery(Long id) {
+        DeliveryResponseDto response = deliveryService.getDelivery(id);
+        return ApiResponse.success(response);
+    }
     // 배송지 수정
     // 배송지 삭제 상태만 변경
     // 기본 배송지 조회

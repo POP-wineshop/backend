bdeddbc1c49ce5e5bf37db68c4043d6e0c449391
Soo186 <rlagustn9797@gmail.com>
2025-06-02 16:21:13 +0900

    refactor : Orderstatus 변경

--- src/main/java/com/popwine/backend/module/order/domain/enums/Orderstatus.java
+++ src/main/java/com/popwine/backend/module/order/domain/enums/Orderstatus.java
@@ -6,8 +6,7 @@ import org.apache.coyote.BadRequestException;
 @Getter
 public enum Orderstatus {
     PENDING("주문 대기중"),
-    CONFIRMED("주문 완료"),
-    COMPLETED("결제 완료"),
+    COMPLETED("결제 완료"), //실 주문 완료
     CANCELED("취소됨");
 
     private final String value;

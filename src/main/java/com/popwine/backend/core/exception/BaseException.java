fe81610a890a0caaea4afa284e997218231907e3
Soo186 <rlagustn9797@gmail.com>
2025-07-16 14:19:45 +0900

    refactor : ErrorCode 자체 errorcode 추가

--- src/main/java/com/popwine/backend/core/exception/BaseException.java
+++ src/main/java/com/popwine/backend/core/exception/BaseException.java
@@ -16,4 +16,5 @@ public class BaseException extends RuntimeException {
         this.errorCode = errorCode;
     }
 
+
 }
--- src/main/java/com/popwine/backend/core/exception/ErrorResponse.java
+++ src/main/java/com/popwine/backend/core/exception/ErrorResponse.java
@@ -1,19 +1,38 @@
 package com.popwine.backend.core.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * ErrorResponse는 API 응답에서 오류 정보를 나타내는 클래스
+ * 이 클래스는 HTTP 상태 코드, 오류 코드, 오류 메시지를 포함
+ * 주로 예외 처리 시 클라이언트에게 오류 정보를 전달하는 데 사용
+ */
+
+@Builder
 @Getter
 @AllArgsConstructor
 public class ErrorResponse {
     private final int status;
+    private final String code;
     private final String message;
 
     public static ErrorResponse from(ErrorCode errorCode) {
-        return new ErrorResponse(errorCode.getStatus(), errorCode.getMessage());
+        return ErrorResponse.builder()
+                .status(errorCode.getStatus())
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
     }
 
     public static ErrorResponse from(BaseException exception) {
-        return new ErrorResponse(exception.getErrorCode().getStatus(), exception.getMessage());
+        ErrorCode code = exception.getErrorCode();
+        return ErrorResponse.builder()
+                .status(code.getStatus())
+                .code(code.getCode())
+                .message(exception.getMessage())
+                .build();
     }
+
 }

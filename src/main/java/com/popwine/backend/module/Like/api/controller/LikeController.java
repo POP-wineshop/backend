bab9f46509ce68a02639871c406b193ae86ea11c
Soo186 <rlagustn9797@gmail.com>
2025-07-23 16:09:49 +0900

    feat LikeController

---
+++ src/main/java/com/popwine/backend/module/Like/api/controller/LikeController.java
@@ -0,0 +1,23 @@
+package com.popwine.backend.module.Like.api.controller;
+
+import com.popwine.backend.core.common.ApiResponse;
+import com.popwine.backend.module.Like.api.dto.LikeRes;
+import com.popwine.backend.module.Like.application.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/likes")
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping("/{wineId")
+    public ApiResponse<LikeRes> toggleLike(Long wineId) {
+        LikeRes response = likeService.toggleLike(wineId);
+        return ApiResponse.success(response);
+    }
+}

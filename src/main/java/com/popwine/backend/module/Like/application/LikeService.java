639863946583b0563c32f6d88db6ca26b53c56a7
Soo186 <rlagustn9797@gmail.com>
2025-07-23 16:09:40 +0900

    feat LikeService

---
+++ src/main/java/com/popwine/backend/module/Like/application/LikeService.java
@@ -0,0 +1,36 @@
+package com.popwine.backend.module.Like.application;
+
+
+import com.popwine.backend.core.security.util.SecurityUtil;
+import com.popwine.backend.module.Like.api.dto.LikeReq;
+import com.popwine.backend.module.Like.api.dto.LikeRes;
+import com.popwine.backend.module.Like.domain.entity.Like;
+import com.popwine.backend.module.Like.domain.repo.LikeRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class LikeService {
+
+    private final LikeRepo likeRepo;
+
+    // 와인 좋아요 저장
+    public LikeRes toggleLike(Long wineID) {
+        Long userId = SecurityUtil.getCurrentUserId();
+
+        Optional<Like> existing = likeRepo.findByUserIdAndWineId(userId, wineID);
+
+        if (existing.isPresent()) {
+            // 이미 좋아요가 존재하는 경우, 좋아요 삭제
+            likeRepo.deleteLike(wineID, userId);
+            return LikeRes.of(wineID, false, likeRepo.countLikes(wineID));
+        } else {
+            // 좋아요가 없는 경우, 좋아요 추가
+            Like newLike = likeRepo.saveLike(wineID, userId);
+            return LikeRes.from(newLike);
+        }
+    }
+}
